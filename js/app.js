1d9243428afd037bdf06b0136ff5ac0b3dee1770
dalazaro <daryl.lazaro@gmail.com>
2017-07-11 14:36:03 -0700

    updated css and js

--- js/app.js
+++ js/app.js
@@ -13,123 +13,133 @@ var pKeycode;
 
 function player1_alert() {
   inProgress = false;
-  setTimeout(function() {
-    alert("Player 1 wins!");
-  }, 5);
+  // setTimeout(function() {
+  $('.winnerAlert').append(`<h1>PLAYER 1 WINS!</h1>`);
+  // }, 5);
 };
 
 function player2_alert() {
   inProgress = false;
-  setTimeout(function() {
-    alert("Player 2 wins!");
-  }, 5);
+  // setTimeout(function() {
+  $('.winnerAlert').append(`<h1>PLAYER 2 WINS!</h1>`);
+  // }, 5);
 };
 
 $(document).ready(function() {
 
-    // function that runs player 1's "car"
-    $(document).on("keydown", function player1_keydown(event) {
+  $("#box101").css("background-color", "#FF332A");
+  $("#box201").css("background-color", "#0047BA");
 
-      qKeycode = event.keyCode;
+  // function that runs player 1's "car"
+  $(document).on("keydown", function player1_keydown(event) {
 
-      //every time player1 presses the Q key (keyCode 81), the red "car" moves 1 column to the right
-      if (qKeycode === 81 && inProgress) {
+    qKeycode = event.keyCode;
 
-        player1_turn += 1;
+    //every time player1 presses the Q key (keyCode 81), the red "car" moves 1 column to the right
+    if (qKeycode === 81 && inProgress) {
 
-        if (player1_turn === 2) {
-          $("#box101").css("background-color", "#202020");
-          $("#box102").css("background-color", "red");
-        } else if (player1_turn === 3) {
-          $("#box102").css("background-color", "#202020");
-          $("#box103").css("background-color", "red");
-        } else if (player1_turn === 4) {
-          $("#box103").css("background-color", "#202020");
-          $("#box104").css("background-color", "red");
-        } else if (player1_turn === 5) {
-          $("#box104").css("background-color", "#202020");
-          $("#box105").css("background-color", "red");
-        } else if (player1_turn === 6) {
-          $("#box105").css("background-color", "#202020");
-          $("#box106").css("background-color", "red");
-        } else if (player1_turn === 7) {
-          $("#box106").css("background-color", "#202020");
-          $("#box107").css("background-color", "red");
-        } else if (player1_turn === 8) {
-          $("#box107").css("background-color", "#202020");
-          $("#box108").css("background-color", "red");
-        } else if (player1_turn === 9) {
-          $("#box108").css("background-color", "#202020");
-          $("#box109").css("background-color", "red");
-        } else if (player1_turn === 10) {
-          $("#box109").css("background-color", "#202020");
-          $("#box110").css("background-color", "red");
-        } else if (player1_turn === 11) {
-          $("#box110").css("background-color", "#202020");
-          $("#box111").css("background-color", "red");
-        } else if (player1_turn === 12) {
-          $("#box111").css("background-color", "#202020");
-          $("#box112").css("background-color", "red");
-          player1_alert();
-        };
+      player1_turn += 1;
 
+      if (player1_turn === 2) {
+        $("#box101").css("background-color", "#202020");
+        $("#box102").css("background-color", "#FF332A");
+      } else if (player1_turn === 3) {
+        $("#box102").css("background-color", "#202020");
+        $("#box103").css("background-color", "#FF332A");
+      } else if (player1_turn === 4) {
+        $("#box103").css("background-color", "#202020");
+        $("#box104").css("background-color", "#FF332A");
+      } else if (player1_turn === 5) {
+        $("#box104").css("background-color", "#202020");
+        $("#box105").css("background-color", "#FF332A");
+      } else if (player1_turn === 6) {
+        $("#box105").css("background-color", "#202020");
+        $("#box106").css("background-color", "#FF332A");
+      } else if (player1_turn === 7) {
+        $("#box106").css("background-color", "#202020");
+        $("#box107").css("background-color", "#FF332A");
+      } else if (player1_turn === 8) {
+        $("#box107").css("background-color", "#202020");
+        $("#box108").css("background-color", "#FF332A");
+      } else if (player1_turn === 9) {
+        $("#box108").css("background-color", "#202020");
+        $("#box109").css("background-color", "#FF332A");
+      } else if (player1_turn === 10) {
+        $("#box109").css("background-color", "#202020");
+        $("#box110").css("background-color", "#FF332A");
+      } else if (player1_turn === 11) {
+        $("#box110").css("background-color", "#202020");
+        $("#box111").css("background-color", "#FF332A");
+      } else if (player1_turn === 12) {
+        $("#box111").css("background-color", "#202020");
+        $("#box112").css("background-color", "#FF332A");
+        player1_alert();
       };
 
-    });
+    };
 
-
-    // //function that runs player 2's "car"
-    $(document).on("keydown", function player2_keydown(event) {
-
-      pKeycode = event.keyCode;
-
-      //every time player2 presses the P key (keyCode 80), the blue "car" moves 1 column to the right
-      if (pKeycode === 80 && inProgress) {
-
-        player2_turn += 1;
-
-        if (player2_turn === 2) {
-          $("#box201").css("background-color", "#202020");
-          $("#box202").css("background-color", "blue");
-        } else if (player2_turn === 3) {
-          $("#box202").css("background-color", "#202020");
-          $("#box203").css("background-color", "blue");
-        } else if (player2_turn === 4) {
-          $("#box203").css("background-color", "#202020");
-          $("#box204").css("background-color", "blue");
-        } else if (player2_turn === 5) {
-          $("#box204").css("background-color", "#202020");
-          $("#box205").css("background-color", "blue");
-        } else if (player2_turn === 6) {
-          $("#box205").css("background-color", "#202020");
-          $("#box206").css("background-color", "blue");
-        } else if (player2_turn === 7) {
-          $("#box206").css("background-color", "#202020");
-          $("#box207").css("background-color", "blue");
-        } else if (player2_turn === 8) {
-          $("#box207").css("background-color", "#202020");
-          $("#box208").css("background-color", "blue");
-        } else if (player2_turn === 9) {
-          $("#box208").css("background-color", "#202020");
-          $("#box209").css("background-color", "blue");
-        } else if (player2_turn === 10) {
-          $("#box209").css("background-color", "#202020");
-          $("#box210").css("background-color", "blue");
-        } else if (player2_turn === 11) {
-          $("#box210").css("background-color", "#202020");
-          $("#box211").css("background-color", "blue");
-        } else if (player2_turn === 12) {
-          $("#box211").css("background-color", "#202020");
-          $("#box212").css("background-color", "blue");
-          player2_alert();
-        };
-
-      };
-
-    });
-
-  $('#reset').click(function() {
-    location.reload();
   });
+
+
+  // //function that runs player 2's "car"
+  $(document).on("keydown", function player2_keydown(event) {
+
+    pKeycode = event.keyCode;
+
+    //every time player2 presses the P key (keyCode 80), the blue "car" moves 1 column to the right
+    if (pKeycode === 80 && inProgress) {
+
+      player2_turn += 1;
+
+      if (player2_turn === 2) {
+        $("#box201").css("background-color", "#202020");
+        $("#box202").css("background-color", "#0047BA");
+      } else if (player2_turn === 3) {
+        $("#box202").css("background-color", "#202020");
+        $("#box203").css("background-color", "#0047BA");
+      } else if (player2_turn === 4) {
+        $("#box203").css("background-color", "#202020");
+        $("#box204").css("background-color", "#0047BA");
+      } else if (player2_turn === 5) {
+        $("#box204").css("background-color", "#202020");
+        $("#box205").css("background-color", "#0047BA");
+      } else if (player2_turn === 6) {
+        $("#box205").css("background-color", "#202020");
+        $("#box206").css("background-color", "#0047BA");
+      } else if (player2_turn === 7) {
+        $("#box206").css("background-color", "#202020");
+        $("#box207").css("background-color", "#0047BA");
+      } else if (player2_turn === 8) {
+        $("#box207").css("background-color", "#202020");
+        $("#box208").css("background-color", "#0047BA");
+      } else if (player2_turn === 9) {
+        $("#box208").css("background-color", "#202020");
+        $("#box209").css("background-color", "#0047BA");
+      } else if (player2_turn === 10) {
+        $("#box209").css("background-color", "#202020");
+        $("#box210").css("background-color", "#0047BA");
+      } else if (player2_turn === 11) {
+        $("#box210").css("background-color", "#202020");
+        $("#box211").css("background-color", "#0047BA");
+      } else if (player2_turn === 12) {
+        $("#box211").css("background-color", "#202020");
+        $("#box212").css("background-color", "#0047BA");
+        player2_alert();
+      };
+
+    };
+
+  });
+
+  $(document).on("keydown", function reset(event) {
+
+    pKeycode = event.keyCode;
+
+    if (qKeycode === 13 && inProgress === false) {
+      location.reload()
+    };
+
+  });
+
 
 });
